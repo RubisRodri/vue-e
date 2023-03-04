36796b65500f46df37f3ede367b3e0b9871af502
Rubis Rodriguez <rubisrodri@gmail.com>
2023-03-04 13:52:41 -0500

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
 <template>
   <div>
-    <v-toolbar
-      app
-      dark
-      prominent
-      image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
-    >
-    
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <Header></Header>
-      <v-toolbar-title class="warning">Vuetify</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-    
-      <v-btn icon>
-        <v-icon>mdi-export</v-icon>
-      </v-btn>
-    </v-toolbar>
+    <header-ru></header-ru>
 
     <v-navigation-drawer app v-model="drawer">
       <h3>Menu vertical</h3>
-      <header-ru></header-ru>
     </v-navigation-drawer>
 
 
--- src/components/HeaderRu.vue
+++ src/components/HeaderRu.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
-    Hola mundo
-  </div>
+    <div>
+      <v-toolbar
+      app
+      dark
+      prominent
+      image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+    
+      <v-app-bar-nav-icon ></v-app-bar-nav-icon>
+      <Header></Header>
+      <v-toolbar-title class="warning">Vuetify</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+    
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    </div>
 </template>
 
